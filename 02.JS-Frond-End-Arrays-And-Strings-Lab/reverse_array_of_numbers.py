7913c12c1aa63e262b468a4af418fa460c7197e8
Funcrow1349 <113984399+Funcrow1349@users.noreply.github.com>
2023-11-05 13:33:49 +0200

    Update

--- 02.JS-Frond-End-Arrays-And-Strings-Lab/reverse_array_of_numbers.py
+++ 02.JS-Frond-End-Arrays-And-Strings-Lab/reverse_array_of_numbers.py
@@ -9,15 +9,3 @@ def reverse_list(elements_count, list_of_numbers):
     print(new_list)
 
 reverse_list(3, [10, 20, 30, 40, 50])
-
-# function reverseArray(elementsCount, arrayOfNumbers) {
-#     const newArray = []
-#     for (let i = 0; i < elementsCount; i++) {
-#         newArray.push(arrayOfNumbers[i])
-#     }
-
-#     const reversedArray = newArray.reverse()
-#     console.log(reversedArray.join(" "))
-# }
-
-# reverseArray(3, [10, 20, 30, 40, 50])
