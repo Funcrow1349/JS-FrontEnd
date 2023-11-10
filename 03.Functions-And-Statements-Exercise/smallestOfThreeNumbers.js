4f9aebbbc1d06329ce03ed35fb365f0c7be554f4
Ivaylo Ivanov <funcrow1349@gmail.com>
2023-11-10 14:17:57 +0200

    Update

---
+++ 03.Functions-And-Statements-Exercise/smallestOfThreeNumbers.js
@@ -0,0 +1,9 @@
+function findSmallestNumber(num1, num2, num3) {
+    let result = Math.min(num1, num2, num3)
+    console.log(result)
+}
+
+findSmallestNumber(2, 5, 3);
+findSmallestNumber(600, 342, 123);
+findSmallestNumber(25, 21, 4);
+findSmallestNumber(2, 2, 2);
