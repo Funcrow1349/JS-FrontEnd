bd077bdda8662398a54472849c4f6746ff1b70e9
Ivaylo Ivanov <funcrow1349@gmail.com>
2023-11-17 09:55:18 +0200

    Update

---
+++ 04.Objects-And-Classes-Lab/personInfo.js
@@ -0,0 +1,11 @@
+function createPersonObject(firstName, lastName, age) {
+    const person = {
+        firstName: firstName,
+        lastName: lastName,
+        age: age,
+    }
+
+    return(person)
+}
+
+createPersonObject("Peter", "Pan", "20")
