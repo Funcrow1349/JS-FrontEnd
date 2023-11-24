89f884ddc99b57292beabf937a5652b1b1e68db7
Funcrow1349 <113984399+Funcrow1349@users.noreply.github.com>
2023-11-24 20:53:56 +0200

    Update

--- 05.JS-Front-End-DOM-Introduction-Lab-Resources/02.Collect List Items/CollectListItems.js
+++ 05.JS-Front-End-DOM-Introduction-Lab-Resources/02.Collect List Items/CollectListItems.js
@@ -1,3 +1,12 @@
 function extractText() {
-    // TODO
+    const elements = document.getElementsByTagName("li")
+    let content = []
+
+    for (const element of Array.from(elements)) {
+        content.push(element.textContent)
+    }
+
+    const textAreaElement = document.getElementById("result")
+    textAreaElement.textContent = content.join("\n")
+    
 }
