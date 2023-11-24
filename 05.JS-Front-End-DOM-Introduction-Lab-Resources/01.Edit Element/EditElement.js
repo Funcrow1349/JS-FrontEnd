039710f2f20284014cb1d8083a5bcfaad07f49a7
Funcrow1349 <113984399+Funcrow1349@users.noreply.github.com>
2023-11-24 20:45:01 +0200

    Update

--- 05.JS-Front-End-DOM-Introduction-Lab-Resources/01.Edit Element/EditElement.js
+++ 05.JS-Front-End-DOM-Introduction-Lab-Resources/01.Edit Element/EditElement.js
@@ -1,3 +1,5 @@
-function editElement() {
-    // TODO
+function editElement(ref, stringMatch, replacer) {
+    const content = ref.textContent
+    const editedText = content.replace(new RegExp(stringMatch, 'g'), replacer)
+    ref.textContent = editedText
 }
