692f9fccd233ab35c3975bdcfda4b4c0868df69d
Funcrow1349 <113984399+Funcrow1349@users.noreply.github.com>
2023-11-25 17:31:26 +0200

    Update

--- 05.JS-Front-End-DOM-Introduction-Lab-Resources/05.Colorize Table/ColorizeTable.js
+++ 05.JS-Front-End-DOM-Introduction-Lab-Resources/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,13 @@
 function colorize() {
-    // TODO
+    const tableContent = document.getElementsByTagName("tr")
+
+    let index = 0
+
+    for (const element of Array.from(tableContent)) {
+        index++
+        if (index % 2 === 0) {
+            element.style.background = 'teal'
+        }
+        
+    }
 }
