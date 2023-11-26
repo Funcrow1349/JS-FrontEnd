e4b01bf6b5634db7257afa25536a4744b12db06e
Funcrow1349 <113984399+Funcrow1349@users.noreply.github.com>
2023-11-26 13:17:57 +0200

    Update

--- 05.JS-Front-End-DOM-Introduction-Lab-Resources/07.Extract Parenthesis/ExtractParenthesis.js
+++ 05.JS-Front-End-DOM-Introduction-Lab-Resources/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,14 @@
 function extract(content) {
+    const text = document.getElementById(content).textContent
+    const regex = /\(([^)]+)\)/g
+    let match = regex.exec(text)
+    let result = []
 
+    while (match !== null) {
+        result.push(match[1])
+        match = regex.exec(text)
+    }
+
+    return result.join("; ")
 }
+
