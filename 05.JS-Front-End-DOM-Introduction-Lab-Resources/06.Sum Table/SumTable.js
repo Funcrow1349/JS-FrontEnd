c8b1262598e8763f4213197aa4c139c5df3cdbbe
Funcrow1349 <113984399+Funcrow1349@users.noreply.github.com>
2023-11-26 13:06:19 +0200

    Update

--- 05.JS-Front-End-DOM-Introduction-Lab-Resources/06.Sum Table/SumTable.js
+++ 05.JS-Front-End-DOM-Introduction-Lab-Resources/06.Sum Table/SumTable.js
@@ -1,3 +1,12 @@
 function sumTable() {
+    const tableContent = document.querySelectorAll("table tr")
+    let totalSum = 0
 
+    for (let index = 1; index < tableContent.length; index++) {
+        const cells = tableContent[index].children
+        const currentPrice = Number(cells[1].textContent)
+        totalSum += currentPrice
+    }
+
+    document.getElementById("sum").textContent = totalSum
 }
