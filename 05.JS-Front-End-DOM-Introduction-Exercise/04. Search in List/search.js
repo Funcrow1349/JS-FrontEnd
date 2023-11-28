38ee7d3f21f3d23f72b51161f1c1fd5d673cffde
Ivaylo Ivanov <funcrow1349@gmail.com>
2023-11-28 10:21:33 +0200

    Update

--- 05.JS-Front-End-DOM-Introduction-Exercise/04. Search in List/search.js
+++ 05.JS-Front-End-DOM-Introduction-Exercise/04. Search in List/search.js
@@ -1,3 +1,22 @@
 function search() {
-   // TODO
+   const listContent = Array.from(document.getElementsByTagName("li"))
+   const searchedText = document.getElementById("searchText").value
+   let result = document.getElementById("result")
+   let matches = 0
+
+   for (const element of listContent) {
+      element.style.textDecoration = "none"
+      element.style.fontWeight = ""
+   }
+
+   for (const element of listContent) {
+      if (element.textContent.includes(searchedText)) {
+         element.style.textDecoration = "underline"
+         element.style.fontWeight = "bold"
+         matches += 1
+      }
+   }
+
+   result.textContent = `${matches} matches found`
+
 }
