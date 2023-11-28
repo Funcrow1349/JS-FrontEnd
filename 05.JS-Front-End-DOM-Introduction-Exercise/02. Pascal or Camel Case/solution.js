4819c2ff60576e4f35cad56808323e73aa3ff28d
Ivaylo Ivanov <funcrow1349@gmail.com>
2023-11-28 09:44:31 +0200

    Update

--- 05.JS-Front-End-DOM-Introduction-Exercise/02. Pascal or Camel Case/solution.js
+++ 05.JS-Front-End-DOM-Introduction-Exercise/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,20 @@
 function solve() {
-  //TODO...
+  let inputText = document.getElementById("text").value;
+  let inputTextArray = inputText.toLowerCase().split(" ");
+  let currentCase = document.getElementById("naming-convention").value;
+  let result = document.getElementById("result");
+
+  if (currentCase === "Camel Case") {
+    const firstWord = inputTextArray.shift();
+    result.innerText += firstWord;
+    for (const word of inputTextArray) {
+      result.innerText += word[0].toUpperCase() + word.slice(1);
+    }
+  } else if (currentCase === "Pascal Case") {
+    for (const word of inputTextArray) {
+      result.innerText += word[0].toUpperCase() + word.slice(1);
+    }
+  } else {
+    result.innerText = "Error!";
+  }
 }
