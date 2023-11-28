fb315fec403f754d3c869af8f37ed879c29e1cd4
Ivaylo Ivanov <funcrow1349@gmail.com>
2023-11-28 11:37:21 +0200

    Update

--- 05.JS-Front-End-DOM-Introduction-Exercise/06. Format the Text/solution.js
+++ 05.JS-Front-End-DOM-Introduction-Exercise/06. Format the Text/solution.js
@@ -1,3 +1,15 @@
 function solve() {
-  //TODO
+  let input = document.getElementById("input").value;
+  let sentences = input.split(".");
+  let output = document.getElementById("output");
+
+  sentences = sentences
+    .filter((s) => s.length > 0)
+    .map((s) => (s += "."));
+
+  while(sentences.length > 0) {
+    let p = document.createElement("p")
+    p.textContent = sentences.splice(0, 3).join(".")
+    output.appendChild(p)
+  }
 }
