d64ef60d48f64aebd35724b8f17240fecdf48c28
Ivaylo Ivanov <funcrow1349@gmail.com>
2023-11-30 12:43:13 +0200

    Update

--- 06.JS-Front-End-DOM-Lab/01. List-Of-Items/app.js
+++ 06.JS-Front-End-DOM-Lab/01. List-Of-Items/app.js
@@ -1,3 +1,9 @@
 function addItem() {
-    console.log('TODO:...');
+    const inputText = document.getElementById("newItemText")
+    const liTag = document.createElement("li")
+    liTag.textContent = inputText.value
+
+    const ulElement = document.getElementById("items")
+    ulElement.appendChild(liTag)
+
 }
