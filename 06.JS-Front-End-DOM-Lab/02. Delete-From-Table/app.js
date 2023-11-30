85b723c6482392829d80f79aea6d1d771e17f9c9
Ivaylo Ivanov <funcrow1349@gmail.com>
2023-11-30 12:57:05 +0200

    Update

--- 06.JS-Front-End-DOM-Lab/02. Delete-From-Table/app.js
+++ 06.JS-Front-End-DOM-Lab/02. Delete-From-Table/app.js
@@ -1,3 +1,17 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    const deleteQuery = document.querySelector("input[name='email']")
+    const result = document.getElementById("result")
+
+    const emails = Array.from(document.querySelectorAll("tbody tr td:nth-child(2)"))
+
+    for (const email of emails) {
+
+        if (email.textContent === deleteQuery.value) {
+            row = email.parentNode
+            row.parentNode.removeChild(row)
+            result.textContent = "Deleted."
+        } else {
+            result.textContent = "Not found."
+        }
+    }
 }
