e42b547dd4ffd38df6aa107531f20a663b17a10d
Ivaylo Ivanov <funcrow1349@gmail.com>
2023-12-01 09:51:11 +0200

    Update

--- 06.JS-Front-End-DOM-Lab/05. Highlight-Active/app.js
+++ 06.JS-Front-End-DOM-Lab/05. Highlight-Active/app.js
@@ -1,3 +1,17 @@
 function focused() {
-    console.log('TODO:...');
+    const inputElements = Array.from(document.getElementsByTagName("input"))
+
+    for (const element of inputElements) {
+        element.addEventListener("focus", addClass)
+        element.addEventListener("blur", removeClass)
+    }
+
+    function addClass(event) {
+        event.currentTarget.parentNode.className = "focused"
+    }
+
+    function removeClass(event) {
+        event.currentTarget.parentNode.classList.remove("focused")
+    }
+    
 }
