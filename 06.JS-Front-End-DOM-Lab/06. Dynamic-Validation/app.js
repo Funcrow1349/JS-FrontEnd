084a89028e7a4f31da9ab7138fbf6487ab3a98d6
Ivaylo Ivanov <funcrow1349@gmail.com>
2023-12-01 10:44:55 +0200

    Update

--- 06.JS-Front-End-DOM-Lab/06. Dynamic-Validation/app.js
+++ 06.JS-Front-End-DOM-Lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,17 @@
 function validate() {
-    console.log('TODO:...');
+    let emailInfo = document.getElementById("email")
+
+    emailInfo.addEventListener("change", validateEmail)
+
+    function validateEmail(event) {
+        const regex = new RegExp(/[a-z]+@[a-z]+\.[a-z]+/g)
+
+        if (!regex.test(emailInfo.value)) {
+            event.currentTarget.className = "error"
+        } else {
+            event.currentTarget.classList.remove("error")
+        }
+
+
+    }
 }
