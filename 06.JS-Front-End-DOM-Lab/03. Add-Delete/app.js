4d180b3906fabb12601656d5e1ff5305e8442bbd
Ivaylo Ivanov <funcrow1349@gmail.com>
2023-12-01 09:35:05 +0200

    Update

--- 06.JS-Front-End-DOM-Lab/03. Add-Delete/app.js
+++ 06.JS-Front-End-DOM-Lab/03. Add-Delete/app.js
@@ -1,3 +1,19 @@
 function addItem() {
-    //TODO...
+    const inputText = document.getElementById("newItemText")
+    const liTag = document.createElement("li")
+    liTag.textContent = inputText.value
+
+    deleteButton = document.createElement("a")
+    deleteButton.textContent = "[Delete]"
+    deleteButton.href = "#"
+    deleteButton.addEventListener("click", removeElement)
+    liTag.appendChild(deleteButton)
+
+    const ulElement = document.getElementById("items")
+    ulElement.appendChild(liTag)
+
+    function removeElement(e) {
+        liTag.remove()
+    }
+
 }
