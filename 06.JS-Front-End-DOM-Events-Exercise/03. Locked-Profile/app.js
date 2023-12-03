e8ef57a5f407a24a5387dd746b1ffc1f547b0312
Funcrow1349 <113984399+Funcrow1349@users.noreply.github.com>
2023-12-03 12:07:55 +0200

    Update

--- 06.JS-Front-End-DOM-Events-Exercise/03. Locked-Profile/app.js
+++ 06.JS-Front-End-DOM-Events-Exercise/03. Locked-Profile/app.js
@@ -1,3 +1,29 @@
 function lockedProfile() {
-    console.log('TODO...')
+    let buttons = Array.from(document.getElementsByTagName("button"))
+
+    for (const button of buttons) {
+        button.addEventListener("click", showMore)
+    }
+
+    function showMore(event) {
+        let btn = event.target
+        let profile = btn.parentElement
+        let children = Array.from(profile.children)
+
+        let unlocked = children[4]
+        let infoToDisplay = children[9]
+
+        if (!unlocked.checked) {
+            return
+        }
+
+        if (btn.textContent === "Show more") {
+            infoToDisplay.style.display = "block"
+            btn.textContent = "Hide it"
+        } else if (btn.textContent === "Hide it") {
+            infoToDisplay.style.display = "none"
+            btn.textContent = "Show more"
+        }
+    }
+
 }
