81130ffa0ed15005ba06f67ee9e648ef0c074f4b
Funcrow1349 <113984399+Funcrow1349@users.noreply.github.com>
2023-12-03 13:08:26 +0200

    Update

--- 06.JS-Front-End-DOM-Events-Exercise/05. Encode-and-Decode-Messages/app.js
+++ 06.JS-Front-End-DOM-Events-Exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,37 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+
+    let sendBtn = document.getElementsByTagName("button")[0]
+    let receiveBtn = document.getElementsByTagName("button")[1]
+
+    let sendText = document.getElementsByTagName("textarea")[0]
+    let receiveText = document.getElementsByTagName("textarea")[1]
+
+    sendBtn.addEventListener("click", encodeAndSendText)
+    receiveBtn.addEventListener("click", decodeAndReadText)
+
+    function encodeAndSendText() {
+        let message = sendText.value
+        let encodedText = ""
+
+        for (let index = 0; index < message.length; index++) {
+            let newChar = String.fromCharCode(message[index].charCodeAt(0) + 1)
+            encodedText += newChar
+        }
+
+        receiveText.value = encodedText
+        sendText.value = ""
+
+    }
+
+    function decodeAndReadText() {
+        let message = receiveText.value
+        let decodedText = ""
+
+        for (let index = 0; index < message.length; index++) {
+            let newChar = String.fromCharCode(message[index].charCodeAt(0) - 1)
+            decodedText += newChar
+        }
+
+        receiveText.value = decodedText
+    }
 }
