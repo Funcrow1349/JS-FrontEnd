b65bf8aba3f1754d81e10c21b501cc5cb9d8f9eb
Funcrow1349 <113984399+Funcrow1349@users.noreply.github.com>
2023-12-03 12:18:23 +0200

    Update

--- 06.JS-Front-End-DOM-Events-Exercise/04. Fill-Dropdown/dropdown.js
+++ 06.JS-Front-End-DOM-Events-Exercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,18 @@
 function addItem() {
-    console.log('TODO:...');
+    let text = document.getElementById("newItemText")
+    let textValue = document.getElementById("newItemValue")
+
+    let menu = document.getElementById("menu")
+
+    let option = document.createElement("option")
+
+    if(text.value !== "" && textValue.value !== "") {
+        option.textContent = text.value
+        option.value = textValue.value
+        menu.appendChild(option)
+
+        text.value = ""
+        textValue.value = ""
+    }
+    
 }
