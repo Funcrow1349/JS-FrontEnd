f4dbb3b7c60e04bc2e9d553fc45c1406f026968c
Ivaylo Ivanov <funcrow1349@gmail.com>
2023-12-04 13:13:28 +0200

    Update

--- 06.JS-Front-End-DOM-Events-Exercise/08. Sudomu/solution.js
+++ 06.JS-Front-End-DOM-Events-Exercise/08. Sudomu/solution.js
@@ -1,4 +1,82 @@
 function solve() {
+  let sudokuRows = Array.from(document.querySelectorAll("tbody tr"));
+  let [checkBtn, clearBtn] = document.querySelectorAll("button");
+  let paragraphMessage = document.querySelector("#check p");
+  let mainTable = document.getElementsByTagName("table")[0];
 
-    //TODO...
+  checkBtn.addEventListener("click", checkSolution);
+  clearBtn.addEventListener("click", clearCells);
+
+  function checkSolution() {
+
+    let row1 = [
+      sudokuRows[0].children[0].children[0].value,
+      sudokuRows[0].children[1].children[0].value,
+      sudokuRows[0].children[2].children[0].value,
+    ];
+
+    let row2 = [
+      sudokuRows[1].children[0].children[0].value,
+      sudokuRows[1].children[1].children[0].value,
+      sudokuRows[1].children[2].children[0].value,
+    ];
+
+    let row3 = [
+      sudokuRows[2].children[0].children[0].value,
+      sudokuRows[2].children[1].children[0].value,
+      sudokuRows[2].children[2].children[0].value,
+    ];
+
+    let column1 = [
+      sudokuRows[0].children[0].children[0].value,
+      sudokuRows[1].children[0].children[0].value,
+      sudokuRows[2].children[0].children[0].value,
+    ];
+
+    let column2 = [
+      sudokuRows[0].children[1].children[0].value,
+      sudokuRows[1].children[1].children[0].value,
+      sudokuRows[2].children[1].children[0].value,
+    ];
+
+    let column3 = [
+      sudokuRows[0].children[2].children[0].value,
+      sudokuRows[1].children[2].children[0].value,
+      sudokuRows[2].children[2].children[0].value,
+    ];
+
+    let setRow1 = new Set(row1);
+    let setRow2 = new Set(row2);
+    let setRow3 = new Set(row3);
+    let setColumn1 = new Set(column1);
+    let setColumn2 = new Set(column2);
+    let setColumn3 = new Set(column3);
+
+    if (
+      setRow1.size < 3 ||
+      setRow2.size < 3 ||
+      setRow3.size < 3 ||
+      setColumn1.size < 3 ||
+      setColumn2.size < 3 ||
+      setColumn3.size < 3
+    ) {
+      paragraphMessage.textContent = "NOP! You are not done yet...";
+      paragraphMessage.style.color = "red";
+      mainTable.style.border = "2px solid red";
+    } else {
+      paragraphMessage.textContent = "You solve it! Congratulations!";
+      paragraphMessage.style.color = "green";
+      mainTable.style.border = "2px solid green";
+    }
+  }
+
+  function clearCells() {
+    paragraphMessage.textContent = "";
+    for (const row of sudokuRows) {
+      row.children[0].children[0].value = "";
+      row.children[1].children[0].value = "";
+      row.children[2].children[0].value = "";
+    }
+    mainTable.style.border = "";
+  }
 }
