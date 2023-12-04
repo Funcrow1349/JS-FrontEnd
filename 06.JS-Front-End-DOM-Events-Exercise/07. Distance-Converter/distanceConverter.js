f85ac4299067fa7bb2e0b1d39b3beb90d8bf7d31
Ivaylo Ivanov <funcrow1349@gmail.com>
2023-12-04 10:33:26 +0200

    Update

--- 06.JS-Front-End-DOM-Events-Exercise/07. Distance-Converter/distanceConverter.js
+++ 06.JS-Front-End-DOM-Events-Exercise/07. Distance-Converter/distanceConverter.js
@@ -1,3 +1,26 @@
 function attachEventsListeners() {
-    console.log('TODO:...');
+    
+    const units = {
+        "km": 1000,
+        "m": 1,
+        "cm": 0.01,
+        'mm': 0.001,
+        "mi": 1609.34,
+        "yrd": 0.9144,
+        "ft": 0.3048,
+        "in": 0.0254,
+    }
+    
+    let convertBtn = document.getElementById("convert")
+    convertBtn.addEventListener("click", convertUnits)
+
+    function convertUnits() {
+        let inputDistance = document.getElementById("inputDistance")
+        let outputDistance = document.getElementById("outputDistance")
+
+        let convertFrom = document.getElementById("inputUnits")
+        let convertTo = document.getElementById("outputUnits")
+
+        outputDistance.value = inputDistance.value * units[convertFrom.value] / units[convertTo.value]
+    }
 }
