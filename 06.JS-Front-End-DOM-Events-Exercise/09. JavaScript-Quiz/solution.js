9c17d6f3e6bec81683c50fdb386482462ecb9041
Ivaylo Ivanov <funcrow1349@gmail.com>
2023-12-05 14:42:54 +0200

    Update

--- 06.JS-Front-End-DOM-Events-Exercise/09. JavaScript-Quiz/solution.js
+++ 06.JS-Front-End-DOM-Events-Exercise/09. JavaScript-Quiz/solution.js
@@ -1,3 +1,28 @@
 function solve() {
-  //TODO...
+  let questions = Array.from(document.getElementsByTagName("section"));
+  let totalCorrectAnswers = 0;
+  let correctAnswers = {
+    1: "onclick",
+    2: "JSON.stringify()",
+    3: "A programming API for HTML and XML documents",
+  };
+  let answerKey = 1;
+
+  questions[0].classList.remove("hidden");
+
+  let [firstAnswerBtn, secondAnswerBtn] = questions[0].getElementsByClassName("answer-wrap");
+  let [firstAnswer, secondAnswer] = questions[0].getElementsByClassName("answer-text");
+  firstAnswerBtn.addEventListener("click", checkAnswer(firstAnswer.textContent));
+  secondAnswerBtn.addEventListener("click", checkAnswer(secondAnswer.textContent));
+
+  function checkAnswer(answer) {
+    if (answer === correctAnswers[answerKey]) {
+      totalCorrectAnswers += 1;
+      answerKey += 1;
+      console.log(answerKey)
+      console.log(totalCorrectAnswers)
+    }
+  }
+
+  
 }
