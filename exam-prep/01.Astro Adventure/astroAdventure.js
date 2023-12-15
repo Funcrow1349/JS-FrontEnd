cf828e68c17d45c38d91c989d410d75bfd7b72a7
Ivaylo Ivanov <funcrow1349@gmail.com>
2023-12-15 08:52:52 +0200

    Update

--- exam-prep/01.Astro Adventure/astroAdventure.js
+++ exam-prep/01.Astro Adventure/astroAdventure.js
@@ -1,1 +1,24 @@
-//TODO...
+function solve(input) {
+    const astronautsCount = Number(input.shift())
+    const astronauts = {}
+
+    for (let index = 0; index < astronautsCount - 1; index++) {
+        const [astronautName, oxygenLevel, energyReserves] = input.shift().split(" ")
+        
+        astronauts[astronautName] = {
+            oxygenLevel: Number(oxygenLevel),
+            energyReserves: Number(energyReserves),
+        }  
+    }
+
+    
+}
+
+solve([  '3',
+'John 50 120',
+'Kate 80 180',
+'Rob 70 150',
+'Explore - John - 50',
+'Refuel - Kate - 30',
+'Breathe - Rob - 20',
+'End'])
